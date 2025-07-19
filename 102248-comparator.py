9da0ca5d9225671757ff482483e47970305670ce
Sina.F <sinaghahreman84@gmail.com>
2025-07-19 16:19:01 +0330

    Add `comparator`
    102248

---
+++ 102248-comparator.py
@@ -0,0 +1,18 @@
+# https://quera.org/problemset/102248
+
+def compare(s1, s2):
+    while s1 and s2:
+        o1 = ord(s1[0])
+        o2 = ord(s2[0])
+        if o1 <= o2:
+            s1 = s1[1:]
+        if o2 <= o1:
+            s2 = s2[1:]
+        if s2:
+            s1 = s1[::-1]
+        if s1:
+            s2 = s2[::-1]
+
+    if s1 == s2 == '':
+        return 'Both strings are empty!'
+    return s1 or s2
