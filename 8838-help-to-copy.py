8c11aac78986e17e012e234d34706051dfd84b38
Sina.F <sina.programerr@gmail.com>
2025-07-22 00:20:10 +0330

    Update `8838-help-to-copy.py`

--- 8838-help-to-copy.py
+++ 8838-help-to-copy.py
@@ -1,9 +1,4 @@
 # https://quera.org/problemset/8838
 
 n, string = input().split()
-
-repeats = ['copy of' for _ in range(int(n))]
-repeats.append(string)
-
-result = ' '.join(repeats)
-print(result)
+print(int(n) * 'copy of ' + string)
