2a0b08e470f24542a28a364c8066ae1896252778
Sina.F <sina.programerr@gmail.com>
2025-07-22 00:58:56 +0330

    Update `221462-missed-point.py`

--- 221462-missed-point.py
+++ 221462-missed-point.py
@@ -1,15 +1,8 @@
 # https://quera.org/problemset/221462
 
-X, Y, Z = [], [], []
+n = 3
+P = [list(map(int, input().split())) for _ in range(2**n - 1)]
+assert all(map(lambda row: len(row) == n, P))
+P = [[P[k][i] for k in range(len(P))] for i in range(n)]  # transpose
 
-for _ in range(7):
-    x, y, z = map(int, input().split())
-    X.append(x)
-    Y.append(y)
-    Z.append(z)
-
-print(
-    min(set(X), key=X.count),
-    min(set(Y), key=Y.count),
-    min(set(Z), key=Z.count)
-)
+print(*(min(P[i], key=P[i].count) for i in range(n)))
