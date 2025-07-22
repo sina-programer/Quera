9697cb64ed6d3adbfb9b13921bd4975ed88336c5
Sina.F <sina.programerr@gmail.com>
2025-07-22 09:37:28 +0330

    Update `291610-phone-number.py`

--- 291610-phone-number.py
+++ 291610-phone-number.py
@@ -4,12 +4,12 @@ n = int(input())
 
 for _ in range(n):
     phone = input()
+    if phone.startswith('09'):
+        phone = '98' + phone[1:]
+    elif phone.startswith('+98'):
+        phone = phone[1:]
 
-    if phone.startswith('09') and phone.isdigit() and len(phone)==11:
-        print('+98' + phone[1:])
-    elif phone.startswith('98') and phone.isdigit() and len(phone)==12:
+    if phone.startswith('98') and phone.isdigit() and len(phone)==12:
         print('+' + phone)
-    elif phone.startswith('+98') and phone[1:].isdigit() and len(phone)==13:
-        print(phone)
     else:
         print('invalid')
