4e86dcc363001417248c9e460260c435176aae9b
Sina.F <sina.programerr@gmail.com>
2025-07-26 10:30:48 +0330

    Update `66864-code-class.py`

--- 66864-code-class.py
+++ 66864-code-class.py
@@ -1,9 +1,21 @@
 # https://quera.org/problemset/66864
 
 k = int(input())
-numbers = ''
 
-for i in range(k+1):
-    numbers += str(i)
+b = 0  # foregoing boundary
+n = 0  # number of digits
+while True:
+    boundary = b + int(n * 9 * (10 ** (n-1)))
+    if k <= boundary:
+        break
+    b = boundary
+    n += 1
 
-print(numbers[k])
+# d: index of desired number between all n-digits
+# m: index of digit in the number
+d, m = divmod(k-b-1, n)
+x = (10 ** (n-1)) + d  # target number
+for _ in range(n-m):
+    xx, digit = divmod(x, 10)
+    x = xx
+print(digit)
