38453e5a71339c3375ca2ea94aa143ea7cd29672
Sina.F <sina.programerr@gmail.com>
2025-08-07 15:45:35 +0330

    Update `275795-print-sum.py`

--- 275795-print-sum.py
+++ 275795-print-sum.py
@@ -1,6 +1,4 @@
 # https://quera.org/problemset/275795
 
 n = int(input())
-nums = range(1, n+1)
-summation = sum(nums)
-print(' + '.join(map(str, nums)), '=', summation)
+print(' + '.join(map(str, range(1, n+1))), '=', int(n*(n+1)/2))
