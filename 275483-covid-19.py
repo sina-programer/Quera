a8bdceff62f8a7ef43b571e6cc358fecbc300b29
Sina.F <sinaghahreman84@gmail.com>
2025-08-07 15:41:11 +0330

    Add `covid-19`
    275483

---
+++ 275483-covid-19.py
@@ -0,0 +1,11 @@
+# https://quera.org/problemset/275483
+
+p = int(input())
+q = int(input())
+
+if (p<=50) and (q<=10):
+    print('White')
+elif q > 30:
+    print('Red')
+else:
+    print('Yellow')
