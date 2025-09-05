2466a5ec82cde8077cccdbfe49c4a73273a88d30
Sina.F <sina.programerr@gmail.com>
2025-09-05 14:09:03 +0330

    Update `76278-mth.py`

--- 76278-mth.py
+++ 76278-mth.py
@@ -1,11 +1,19 @@
 # https://quera.org/problemset/76278
 
-def calculator(n, m, array):
-    value = 0
+def windowise(array, w):
     i = 0
-    ceil = n // m
-    while i <= ceil:
-        value += sum(array[i*m : (i+1)*m]) * (-1) ** (i%2)
+    while True:
+        sub = array[i*w : (i+1)*w]
+        if sub:
+            yield sub
+        else:
+            break
         i += 1
 
-    return value
+def calculator(n, m, array):
+    return sum(
+        [
+            sum(window) * ((-1) ** i)
+            for i, window in enumerate(windowise(array, m))
+        ]
+    )
